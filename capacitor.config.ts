1e694ee14d19b14af24d036b58e2f0b0c9fa6c2b
vicky435435 <vickymaneshwar435@gmail.com>
2023-06-29 12:03:06 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'file-uploadfb',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
